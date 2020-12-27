841e485717033871a1b47b16dc12515725d964e6
Nathan Abela <nathan.abela.e25765@mcast.edu.mt>
2020-12-27 13:58:49 +0100

    Add 2D Car Game Setup
    - Setup Scene
    - Setup Camera
    - Setup Border around camera

---
+++ Home Assignment/2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // SerializeField makes the variable editable from Unity
+    [SerializeField] float padding = 1f;
+
+    float xMin, xMax, yMin, yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Setting up a border around the camera
+    private void SetUpMoveBoundaries()
+    {
+        // Retrieving camera from Unity
+        Camera gameCamera = Camera.main;
+
+        // Minimum and Maximum position of the camera
+        // xMin = 0, xMax = 1
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        // yMin = 0, yMax = 1
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
