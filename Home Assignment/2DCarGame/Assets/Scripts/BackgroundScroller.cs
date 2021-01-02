6935cb58e368adf2956f767df500a792490c80d9
Nathan Abela <nathan.abela.e25765@mcast.edu.mt>
2021-01-03 00:12:24 +0100

    Add Scrolling Background
    - Remove Background Prefab
    - Add Quad as a Background

---
+++ Home Assignment/2DCarGame/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    // Speed of the scrolling
+    [SerializeField] float backgroundScrollSpeed = 1.5f;
+
+    // Material from the texture
+    Material myMaterial;
+
+    // offSet Movement
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get material of the background from the Renderer component
+        myMaterial = GetComponent<Renderer>().material;
+
+        // Scroll in the y-axis at the speed
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the texture of the material by offSet every frame
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
